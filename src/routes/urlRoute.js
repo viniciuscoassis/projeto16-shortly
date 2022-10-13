b5a1d02b527a0b8a08aba73b9e079bf83b23c440
vinicius <vinicius.coassis@gmail.com>
2022-10-13 16:37:29 -0300

    feat: add search by id route

--- src/routes/urlRoute.js
+++ src/routes/urlRoute.js
@@ -9,9 +9,11 @@ const urlRoute = express.Router();
 urlRoute.get("/urls/:id", async (req, res) => {
   const { id } = req.params;
 
-  const idUrl = await connection.query("SELECT * FROM urls WHERE id = $1", [
-    id,
-  ]);
+  const idUrl = await connection.query(
+    'SELECT id,"shortUrl",url FROM urls WHERE id = $1',
+    [id]
+  );
+  if (idUrl.rows.length == 0) return res.sendStatus(404);
 
   res.send(idUrl.rows[0]);
 });
